2c2dc2d96aea83119595d070c30a9f014c634fd1
朝贺 Choga <357912221@protonmail.com>
2024-07-16 11:51:42 +1200

    feat : added store page card

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,21 @@
-import {Button, Container, Navbar as NavbarBs, NavLink} from "react-bootstrap"
+import { Button, Container, Navbar as NavbarBs } from "react-bootstrap"
+import { NavLink, Link } from "react-router-dom"
 import { Nav } from "react-bootstrap"
 
-export function Navbar(){
-    
-    return(
-        <NavbarBs className="bg-white shadow-sm mb-3">
+export function Navbar() {
+    return (
+        <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
             <Container>
                 <Nav className="me-auto">
-                    <Nav.Link to="/" as={NavLink}>Home</Nav.Link>
-                    <Nav.Link to="/store" as={NavLink}>Store</Nav.Link>
-                    <Nav.Link to="/about" as={NavLink}>About</Nav.Link>
+                    <Nav.Link as={Link} to="/">Home</Nav.Link>
+                    <Nav.Link as={Link} to="/store">Store</Nav.Link>
+                    <Nav.Link as={Link} to="/about">About</Nav.Link>
                 </Nav>
-                <Button style={{width:"3rem",height:"3rem"}}variant="outline-primary" className="rounded-circle">
-                <img src="/public/shopping-cart.svg" width="25" height="25"></img>
+                <Button style={{ width: "3rem", height: "3rem", position: "relative" }} variant="outline-primary" className="rounded-circle">
+                    <img src="/public/shopping-cart.svg" width="25" height="25" alt="Shopping Cart" />
+                    <div className="rounded-circle bg-danger d-flex justify-content-center align-items-center" style={{ color: "white", width: "1.5rem", height: "1.5rem", position: "absolute", bottom: 0, right: 0, transform: "translate(25%, 25%)" }}>3</div>
                 </Button>
             </Container>
         </NavbarBs>
-    ) 
+    )
 }
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,18 @@
-export function Store(){
-    return(
-        <h1>Store</h1>
-    )
+import storeItems from "../data/items.json";
+import { Row, Col } from "react-bootstrap";
+import { StoreItem } from "../components/StoreItem.tsx"
+
+export function Store() {
+    return (
+        <>
+            <h1>Store</h1>
+            <Row md={2} xs={1} lg={3} className="g-3">
+                {storeItems.map(item => (
+                    <Col key={item.id}>
+                        <StoreItem {...item} />
+                    </Col>
+                ))}
+            </Row>
+        </>
+    );
 }
